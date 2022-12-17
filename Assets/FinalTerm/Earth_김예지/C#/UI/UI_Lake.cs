e3a6a45c2312f7d4f9448b667cecfb857f9c8824
YeJiKim06 <erosiezz@gmail.com>
2022-12-17 18:12:56 +0900

    UI 5

---
+++ Assets/FinalTerm/Earth_김예지/C#/UI/UI_Lake.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Lake : MonoBehaviour
+{
+    public GameObject MyGameObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        string name = MyGameObject.name;
+        name += "School";
+        GetComponent<Text>().text = MyGameObject.name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
