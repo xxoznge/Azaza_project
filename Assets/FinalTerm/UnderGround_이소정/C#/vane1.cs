934d44c59fc0ec4fbbd10e0c35e45f29a9693f12
Sojung Lee <90453158+xxoznge@users.noreply.github.com>
2022-12-17 20:38:34 +0900

    vane

--- Assets/FinalTerm/UnderGround_이소정/C#/vane1.cs
+++ Assets/FinalTerm/UnderGround_이소정/C#/vane1.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Vane : MonoBehaviour
+public class vane1 : MonoBehaviour
 {
     // Start is called before the first frame update
     Animator Anim;
