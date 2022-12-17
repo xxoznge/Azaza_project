8efc84ea41f51b60c2f3fa68d0ac45f0e907ba6d
Sojung Lee <90453158+xxoznge@users.noreply.github.com>
2022-12-17 20:08:35 +0900

    camera_mirror

--- Assets/FinalTerm/UnderGround_이소정/C#/Vane2.cs
+++ Assets/FinalTerm/UnderGround_이소정/C#/Vane2.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Vane : MonoBehaviour
+public class Vane2 : MonoBehaviour
 {
     // Start is called before the first frame update
     Animator Anim;
