5828654fdc8ac5fe054209c3c093c06bc2f61382
YeJiKim06 <erosiezz@gmail.com>
2022-12-18 22:51:11 +0900

    6

--- Assets/FinalTerm/UI/To_UnderGround.cs
+++ Assets/FinalTerm/UI/To_UnderGround.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class U_Load_UI_Scene : MonoBehaviour
+public class To_UnderGround : MonoBehaviour
 {
     public Object SceneToLoad;
     Scene CurrentScene;
--- Assets/FinalTerm/UI/toUnderGround.cs
+++ Assets/FinalTerm/UI/toUnderGround.cs
@@ -4,16 +4,9 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class U_Load_UI_Scene : MonoBehaviour
+public class toUnderGround : MonoBehaviour
 {
     public Object SceneToLoad;
-    Scene CurrentScene;
-
-    void Start()
-    {
-        CurrentScene = gameObject.scene;
-        print("CurrentScene = " + CurrentScene.name);
-    }
     public void OnReTry()
     {
         if (Input.GetKeyDown(KeyCode.U))
@@ -26,3 +19,4 @@ public class U_Load_UI_Scene : MonoBehaviour
         }
     }
 }
+
